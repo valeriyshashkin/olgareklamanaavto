3bbd373e8dc46173f67c142b400aded95f70c32b
valeriyshashkin <velianjanjan@gmail.com>
2021-11-25 19:13:40 +0500

    заголовок, разделение на компоненты, начало работы над галереей

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,43 @@
 import Calculator from "../components/Calculator";
+import Head from "next/head";
 
-export default function Index() {
+function Subtitle({ children }) {
   return (
     <>
+      <h1>{children}</h1>
+      <style jsx>{`
+        h1 {
+          font-size: 40px;
+        }
+      `}</style>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Head>
+        <title>Olga.ru - креативная оклейка автомобилей</title>
+      </Head>
       <header>
         <nav>
-          <h1 className="logo">Olga.ru</h1>
+          <h1>
+            <a href="#">Olga.ru</a>
+          </h1>
           <ul>
-            <li>Калькулятор</li>
-            <li>Сделано</li>
-            <li>Обращайтесь</li>
+            <li>
+              <a href="#">Калькулятор</a>
+            </li>
+            <li>
+              <a href="#gallery">Сделано</a>
+            </li>
+            <li>
+              <a href="#contacts">Обращайтесь</a>
+            </li>
           </ul>
         </nav>
       </header>
-      <section className="calculator">
-        <Calculator />
-      </section>
-      <section className="gallery"></section>
-      <section className="contacts"></section>
       <style jsx>{`
         header {
           border-bottom: 1px solid gray;
@@ -46,6 +66,27 @@ export default function Index() {
           margin-right: 0;
         }
 
+        a {
+          text-decoration: none;
+          color: black;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <section>
+        <Calculator />
+      </section>
+      <section id="gallery">
+        <Subtitle>Наклейки, воплощенные в реальность</Subtitle>
+      </section>
+      <section id="contacts"></section>
+      <style jsx>{`
         section {
           width: 1024px;
           margin: 0 auto;
