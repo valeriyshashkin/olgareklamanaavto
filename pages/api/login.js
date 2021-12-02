83324f4e28ba24117d5c47fec9d3c1ddd190052e
valeriyshashkin <velianjanjan@gmail.com>
2021-12-02 21:31:19 +0500

    добавил модели админов, картинок и контента

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,11 +4,6 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     res.status(200).json({ error: false });
   } else {
-    const db = await connectDb();
-    await db.testModel.create({
-      test: "new test",
-    });
-    const result = await db.testModel.findAll();
-    res.status(200).json({ testModels: result });
+    res.status(200).json({ ok: true });
   }
 }
