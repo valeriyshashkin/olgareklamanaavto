db791ecea3e76f9449d6b3528fa70a61bf39b3b5
valeriyshashkin <velianjanjan@gmail.com>
2021-12-24 21:02:24 +0500

    начал делать галерею в админке

--- components/Button.js
+++ components/Button.js
@@ -1,23 +1,28 @@
-export default function Button({ large, onClick, children, margin }) {
+export default function Button({ large, onClick, children, margin, labelFor }) {
   return (
-    <button onClick={onClick}>
-      {children}
+    <>
+      {labelFor ? (
+        <label htmlFor={labelFor} onClick={onClick}>{children}</label>
+      ) : (
+        <button onClick={onClick}>{children}</button>
+      )}
       <style jsx>{`
-        button {
+        button, label {
           background: var(--to-color);
           border: none;
           color: white;
           border-radius: 6px;
           padding: 10px;
           cursor: pointer;
+          display: inline-block;
         }
       `}</style>
       <style jsx>{`
-        button {
+        button, label {
           margin: ${margin ?? "0"};
           font-size: ${large ? "20px" : "14px"};
         }
       `}</style>
-    </button>
+    </>
   );
 }
--- pages/admin/gallery.js
+++ pages/admin/gallery.js
@@ -1,8 +1,26 @@
 import Head from "next/head";
 import Layout from "../../components/Layout";
+import Button from "../../components/Button";
 
 export default function Gallery() {
-  return <>nothing...</>;
+  return (
+    <>
+      <input id="file-upload" type="file" multiple accept="image/*" />
+      <Button margin="12px 0" labelFor="file-upload">
+        Добавить фото
+      </Button>
+      <style jsx>{`
+        #file-upload {
+          opacity: 0;
+          width: 0;
+        }
+
+        label {
+          height: 100%;
+        }
+      `}</style>
+    </>
+  );
 }
 
 Gallery.getLayout = function getLayout(page) {
