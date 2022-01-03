3acca75a30c53fa4836efa48a7fec5b8b5657c50
valeriyshashkin <velianjanjan@gmail.com>
2022-01-04 00:57:58 +0500

    добавил галочку вместо точки

--- components/Price.js
+++ components/Price.js
@@ -1,30 +1,53 @@
 export default function Price({ title, price, features }) {
   return (
     <>
-      <div>
+      <div className="container">
         <p className="title">{title}</p>
         <p className="price">от {price} рублей</p>
-        <ul>
-          {features.map((f, i) => (
-            <li key={i}>{f}</li>
-          ))}
-        </ul>
+        {features.map((f, i) => (
+          <div className="item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+            </svg>
+            <div key={i}>{f}</div>
+          </div>
+        ))}
       </div>
       <style jsx>{`
-        div {
+        .container {
           max-width: 300px;
           padding: 0 20px;
         }
 
-        div:first-child,
-        div:last-child {
+        .container:last-child {
           margin: 0;
+        }
+
+        .item {
+          display: flex;
+          margin: 20px 0;
+          align-items: center;
+        }
+
+        .item svg {
+          fill: var(--to-color);
+          min-width: 24px;
+        }
+
+        .item div {
+          margin-left: 10px;
         }
 
         .title {
           font-size: 24px;
           margin: 0;
-          font-weight: bold;
+          font-weight: 600;
         }
 
         .price {
@@ -43,10 +66,16 @@ export default function Price({ title, price, features }) {
             font-size: 18px;
           }
         }
+
+        @media (max-width: 580px) {
+          .container {
+            margin-bottom: 40px;
+          }
+        }
       `}</style>
       <style jsx>{`
         @media (max-width: 720px) {
-          div {
+          .container {
             flex-direction: column;
             margin: 0;
           }
