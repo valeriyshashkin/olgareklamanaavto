cdeb982c5c552ba8266fa74971ec68c710447b72
valeriyshashkin <velianjanjan@gmail.com>
2022-06-08 15:11:02 +0500

    редизайн слайдера

--- components/Slider.js
+++ components/Slider.js
@@ -7,6 +7,8 @@ import {
   ArrowCircleLeftIcon,
   XIcon,
   TrashIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/outline";
 import classNames from "classnames";
 import { useEffect } from "react";
@@ -68,17 +70,17 @@ export default function Slider({ images, onClick, currentSlide, preview }) {
     >
       <div
         onClick={onClick}
-        className="flex items-center justify-center w-9 h-9 cursor-pointer absolute right-0 top-0 m-4 sm:m-8 bg-white rounded-full z-20"
+        className="group absolute top-0 right-0 z-30 cursor-pointer w-20 h-20 sm:w-40 sm:h-40 flex items-center justify-center"
       >
-        <XIcon className="w-7 h-7" />
+        <XIcon className="h-10 stroke-gray-500 group-hover:stroke-white" />
       </div>
       {preview && (
         <>
           <label
             htmlFor="delete"
-            className="cursor-pointer z-20 absolute border-2 border-red-500 right-0 w-9 h-9 flex items-center justify-center rounded-full m-4 mr-16 sm:m-8 sm:mr-24"
+            className="group cursor-pointer h-20 w-20 sm:h-40 sm:w-40 z-30 absolute right-0 top-0 mr-20 sm:mr-40 flex items-center justify-center"
           >
-            <TrashIcon className="w-7 h-7 stroke-red-500" />
+            <TrashIcon className="h-10 group-hover:stroke-red-500 stroke-red-400" />
           </label>
           <input type="checkbox" id="delete" className="modal-toggle" />
           <label htmlFor="delete" className="modal cursor-pointer">
@@ -107,16 +109,20 @@ export default function Slider({ images, onClick, currentSlide, preview }) {
         </>
       )}
       {!isFirst && (
-        <ArrowCircleLeftIcon
+        <div
           onClick={prev}
-          className="hidden sm:block z-20 w-10 h-10 cursor-pointer absolute bg-white rounded-full top-1/2 -translate-y-1/2 ml-8"
-        />
+          className="hidden sm:block group absolute top-0 bottom-0 left-0 w-40 cursor-pointer z-20"
+        >
+          <ChevronLeftIcon className="group-hover:stroke-white stroke-gray-500 h-10 top-1/2 -translate-y-1/2 absolute w-40" />
+        </div>
       )}
       {!isLast && (
-        <ArrowCircleRightIcon
+        <div
           onClick={next}
-          className="hidden sm:block z-20 w-10 h-10 cursor-pointer absolute bg-white rounded-full top-1/2 -translate-y-1/2 right-0 mr-8"
-        />
+          className="hidden sm:block group absolute top-0 bottom-0 right-0 w-96 cursor-pointer z-20"
+        >
+          <ChevronRightIcon className="group-hover:stroke-white stroke-gray-500 h-10 top-1/2 -translate-y-1/2 right-0 absolute w-40" />
+        </div>
       )}
       <Swiper
         onSwiper={handleSwiper}
